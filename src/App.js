f40ad2365749e142124b15ef0de37023035bd3cc
Kholoud Awad <kholoudmawad@gmail.com>
2020-04-22 22:44:13 -0400

    removed the robots file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import SearchBox from './SearchBox';
 import CardList from './CardList';
- import {robots} from './robots' ;
+// import {robots} from './robots' ;
 import ErrorBoundary from './ErrorBoundary'
 import Scroll from './Scroll'
 import './App.css'
@@ -14,18 +14,21 @@ class App extends React.Component {
 			searchfield : '' 
 		}
 	}
+
+	componentDidMount () {
+	fetch('http://jsonplaceholder.typicode.com/users')
+	.then(response => response.json())
+	.then (data => this.setState({robots:data}))
+	.catch(console.log);
+}
+
 onSearchChange = (event) => {
 	this.setState({searchfield : event.target.value})
 	
 	// console.log(filteredRobots);
 }
 
-componentDidMount () {
-	fetch('http://jsonplaceholder.typicode.com/users')
-	.then(response => response.json())
-	.then (data => this.setState({robots:data}))
-	.catch(console.log);
-}
+
 	
 	render(){
 		const filteredRobots = this.state.robots.filter(robot => {
@@ -33,7 +36,7 @@ componentDidMount () {
 	})
 		return (
 		<div className='tc'>
-		 <h1 className='f2 head'>RoboFriendsss</h1>
+		 <h1 className='f2 head'>RoboFriendss</h1>
 		 <SearchBox searchchange={this.onSearchChange} />
 		 <Scroll>
 		 <ErrorBoundary>
